161c3c7aef6d7c4ad6db7a02051cd894670c6ebc
pbolaines <98132625+pbolaines@users.noreply.github.com>
2023-11-16 14:43:36 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "instance_type" {
   default = "t3.micro"
 }
-
+ 
 variable "environment" {
   default = "DevOps"
 }
-
+ 
 variable "ec2_key" {
   default = "key"
 }
-
+ 
 variable "ec2_iam_instance_profile" {
-  default = "DevOps_my_2023_ec2_profile"
+  default = "DevOps-ariuna_2023_ec2_profile"
 }
